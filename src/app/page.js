f88e964b20ca286004fb87fc40de27602fa1236b
Muhammad Ahmad <prince1ahmed111@gmail.com>
2024-10-30 15:36:57 +0500

    link changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,33 +3,25 @@ import TodoList from "@/Components/TodoList";
 
 // Server Component
 export default async function Home() {
-  // Fetching todo data
-  let fetchData = await fetch(`${process.env.BASE_URL}`, {
-    cache: "no-cache",
-  });
+  const baseUrl = process.env.BASE_URL || "http://localhost:3000/api/todo";
+  const baseUrl2 = process.env.BASE_URL_2 || "http://localhost:3000/api/user";
+
+  let fetchData = await fetch(baseUrl, { cache: "no-cache" });
   let jsonDataTodo = await fetchData.json();
 
-  // Fetching user data (if needed)
-  let fetchUser = await fetch(`${process.env.BASE_URL_2}`, {
-    cache: "no-cache",
-  });
+  let fetchUser = await fetch(baseUrl2, { cache: "no-cache" });
   let jsonUser = await fetchUser.json();
 
-console.log(jsonUser);
-
-  
   return (
     <section className="flex flex-col justify-center items-center min-h-screen">
       <h1 className="font-bold text-3xl">Todo App</h1>
-      <Form allUsers={jsonUser}/>
+      <Form allUsers={jsonUser} />
       {/* Pass the fetched data to TodoList component */}
-      {
-        jsonDataTodo && jsonDataTodo.length > 0 ? (
-          <TodoList todo={jsonDataTodo} jsonUser={jsonUser}/>
-        ) : (
-          <p>Loading</p>
-        )
-      }
+      {jsonDataTodo && jsonDataTodo.length > 0 ? (
+        <TodoList todo={jsonDataTodo} jsonUser={jsonUser} />
+      ) : (
+        <p>Loading</p>
+      )}
     </section>
   );
 }
